c2302ebb51dee2d619cd09c87f31f0fa1df86853
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2024-05-07 15:14:36 -0300

    add producer config

--- payment-service/src/main/java/com/jonathan/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/jonathan/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package com.jonathan.paymentservice.service.impl;
 
 import com.jonathan.paymentservice.model.Payment;
 import com.jonathan.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
